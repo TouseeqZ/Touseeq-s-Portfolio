6f348c7ec71b292be73c63e5016eeae16ccba8d3
Touseeq Haider <syedtouseeqhaider@gmail.com>
2024-04-29 13:31:56 +0500

    1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,13 @@
 import Link from 'next/link'
 import  React,{useState} from 'react'
 import NavLink from './NavLink'
-import {Bars3Icon,XMarkIcon} from "@heroicons/react/24/solid";
+
+import { ArrowRightIcon } from '@heroicons/react/24/solid'; // For solid icons
+import { StarIcon } from '@heroicons/react/24/outline'; // For outline icons
+
 import MenuOverlay from './MenuOverlay';
-import Image from 'next/image';
+
+
 const navLinks=[
     {
         title: 'About',
@@ -34,13 +38,13 @@ const Navbar=()=>{
                         !navbarOpen?(
                             <button
                              onClick={()=> setNavbarOpen(true)} className=' flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                                <Bars3Icon className='h-5 w-5'/>
+                                <ArrowRightIcon className='h-5 w-5'/>
                             </button>
 
                         ) : (
                             <button
                              onClick={()=> setNavbarOpen(false)}className=' flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
-                                <XMarkIcon className='h-5 w-5'/>
+                                <StarIcon className='h-5 w-5'/>
                             </button>
                         )
                     }
