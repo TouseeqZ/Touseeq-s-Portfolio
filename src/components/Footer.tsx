2e165eda92ec534874f64a0680811a118fbd67d6
Touseeq Haider <syedtouseeqhaider@gmail.com>
2024-04-29 12:25:31 +0500

    1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span><Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
-            <Image 
-          src="/horse.png"
-          alt="pic"
-          height={100}
-          width={100}
-          className="absolute right-150 top-50 rounded-full"
-        />
-        <Image src="/cliff.webp" alt="cliff" width={50} height={50} />
-
+            Home
                 </Link></span>
         <p className="text-slate-600">Copyright © All Rights Reserved |Touseeq Haider</p>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,14 +26,7 @@ const Navbar=()=>{
         <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2' >
             <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
-            <Image 
-          src="/horse.png"
-          alt="pic"
-          height={100}
-          width={100}
-          className="absolute right-150 top-50 rounded-full"
-        />
-        <Image src="/cliff.webp" alt="cliff" width={50} height={50} />
+            Home
 
                 </Link>
                 <div className='mobile-menu block md:hidden'>
