01c6ebf9a53a486609ca9022f427aad5a313303a
Touseeq Haider <syedtouseeqhaider@gmail.com>
2024-04-29 12:12:57 +0500

    1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,3 +18,4 @@ module.exports = {
   },
   plugins: [],
 };
+export default config;
