efea0f9af9b9e24741a8d3e031f30a77df8ce169
Touseeq Haider <syedtouseeqhaider@gmail.com>
2024-04-29 22:04:07 +0500

    1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Image from "next/image";
 
 const navLinks = [
   {
@@ -30,7 +31,15 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
+         <Image 
+          src="/horse.png"
+          alt="pic"
+          height={100}
+          width={100}
+          className="absolute right-150 top-50 rounded-full"
+        />
+        <Image src="/cliff.webp" alt="cliff" width={50} height={50} />
+
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
