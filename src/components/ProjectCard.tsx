f68d65a8eac6e6100f3f9ea656f945c8462ad6e4
Touseeq Haider <syedtouseeqhaider@gmail.com>
2024-04-29 22:23:04 +0500

    1

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-//import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { ArrowRightIcon } from '@heroicons/react/24/solid'; // For solid icons
-import { StarIcon } from '@heroicons/react/24/outline'; // For outline icons
 interface ProjectCardProps {
-    imgUrl: string;
-    title: string;
-    description: string;
-    gitUrl: string;
-    previewUrl: string;
-  }
-  
-  const ProjectCard = ({
-    imgUrl,
-    title,
-    description,
-    gitUrl,
-previewUrl
-  }: ProjectCardProps
-) => {
+  imgUrl: string;
+  title: string;
+  description: string;
+  gitUrl: string;
+  previewUrl: string;
+}
+
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }:ProjectCardProps) => {
   return (
     <div>
       <div
@@ -30,13 +21,13 @@ previewUrl
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
-            <ArrowRightIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
-            <StarIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
